4432fa18e7cecf34b69c4e2d83c4fba5da712eec
Pavanp1 <x.men99p1@gmail.com>
2022-05-20 15:47:32 +0100

    Update main.tf
    commented locals block for resource-group

--- modules/AWS-SSO/Permission_set/main.tf
+++ modules/AWS-SSO/Permission_set/main.tf
@@ -5,13 +5,13 @@ locals {
     module  = basename(path.module)
     name    = var.name
   }
-    module_tags = var.module_tags_enabled ? {
-    "module.terraform.io/package"   = local.metadata.package
-   # "module.terraform.io/version"   = local.metadata.version
-    "module.terraform.io/name"      = local.metadata.module
-    "module.terraform.io/full-name" = "${local.metadata.package}/${local.metadata.module}"
-    "module.terraform.io/instance"  = local.metadata.name
-  } : {}
+#     module_tags = var.module_tags_enabled ? {
+#     "module.terraform.io/package"   = local.metadata.package
+#    # "module.terraform.io/version"   = local.metadata.version
+#     "module.terraform.io/name"      = local.metadata.module
+#     "module.terraform.io/full-name" = "${local.metadata.package}/${local.metadata.module}"
+#     "module.terraform.io/instance"  = local.metadata.name
+#   } : {}
 
 }
 
